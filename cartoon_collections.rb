b3f55d842fd2be0c41149739dd26834f280c5917
Bryam Vicente <vicentebryam@gmail.com>
2020-08-15 21:59:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
 
   array.each_with_index do |greeting, index|
-    puts "#{index}. #{greeting}\n1\n1"
+    puts "#{index}. #{greeting}"
 
 end
 end
